b559356767cee5c8b0093bbac0c7eebff8178d43
codewtzjx <2870898907@qq.com>
2020-05-07 00:23:19 +0800

    gmall需求第2次提交

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gmall0105
+
+gmall-user-service:8070
+
+gmall-user-web:8080
---
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.user.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    @Transactional
+    public List<UmsMember> getAllUser() {
+        List list = userMapper.selectAll();
+        return list;
+    }
+}
